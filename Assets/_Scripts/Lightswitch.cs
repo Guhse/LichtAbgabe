6e452c1ef26c7574d3decb060873e3af9d7617ae
Guhse <guhselukas@gmail.com>
2019-05-03 13:13:09 +0200

    commit neu

---
+++ Assets/_Scripts/Lightswitch.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class Lightswitch : MonoBehaviour
+{
+    [SerializeField] public GameObject Lights;
+    [SerializeField] public GameObject Player;
+
+    private bool enter = true;
+
+    private void Start()
+    {
+        
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (enter == true)
+        {
+            GetComponent<Light>().enabled = true;
+           
+        }
+    }
+
+}
